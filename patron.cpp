f238470e4cb23e83c748c4bb75096a6c2c056ea0
Ray <119020664+Goos8855@users.noreply.github.com>
2026-03-06 23:51:47 +0000

    most of the patron functions completed

--- patron.cpp
+++ patron.cpp
@@ -1,21 +1,43 @@
 #include "patron.h"
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-//patron object
-//borrowBook()
-//returnBook()
-//displayPatron()
+
 //operator==
 //operator<<
 
+//Patron Object: stores name, id , and books borrowed
 Patron::Patron(string name, int id): name(name), id(id){}
 
-void borrowBook(Book* b){
+//borrowBook: checks for availability then removes from available library to add it to patron's library
+void Patron::borrowBook(Book* b){
     if(b->getStatus()==BookStatus::Available){
         b->setStatus(BookStatus::CheckedOut);
-        borrowBook.push_back(b);
+        borrowedBooks.push_back(b);
     } else {
         cout << "Book Unavailable" << endl;
+    }
+}
+
+//returnBook: checks for book in patron's library then adds it back to the available library
+void Patron::returnBook(Book* b){
+    for(int i=0; i<borrowedBooks.size(); i++){
+        if(borrowedBooks[i] == b){
+            b->setStatus(BookStatus::Available);
+            borrowedBooks.erase(borrowedBooks.begin() + i);
+            break;
+        }
+    }
+    cout << "Book not borrowed" << endl;
+}
+
+//displayPatron: prints name, id, and books borrowed in terminal
+void Patron::displayPatron() const{
+    cout << "Patron #" << id << ": " << name << endl;
+    cout << borrowedBooks.size() << " Borrowed Books: " << endl;
+    for(int i=0; i<borrowedBooks.size(); i++){
+        cout << borrowedBooks[i] << endl;
     }
 }
